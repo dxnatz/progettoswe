fbb6dec4ef263857a69d250e5fd583361a97470b
gabriele-cmd <75305819+gabriele-cmd@users.noreply.github.com>
2025-03-03 18:18:48 +0100

    aggiunta la logica e i controlli per il login

--- src/main/java/com/progettoswe/LoginController.java
+++ src/main/java/com/progettoswe/LoginController.java
@@ -1,8 +1,14 @@
 package com.progettoswe;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import java.io.IOException;
 
@@ -13,7 +19,7 @@ public class LoginController {
     private TextField mailTextField;
 
     @FXML
-    private TextField passwordField;
+    private TextField passwordTextField;
 
     @FXML
     private Button loginButton;
@@ -21,7 +27,7 @@ public class LoginController {
     @FXML
     private void handleLogin() {
         String mail = mailTextField.getText();
-        String password = passwordField.getText();
+        String password = passwordTextField.getText();
             
 
         
@@ -38,8 +44,35 @@ public class LoginController {
         App.setRoot("registrate");
     }
 
-    private boolean authenticate(String username, String password) {
-        // Placeholder for authentication logic
-        return "admin".equals(username) && "password".equals(password);
+    private boolean authenticate(String email, String password) {
+        // Implement authentication logic here
+        String query = "SELECT count(*) FROM utente WHERE email = ? AND pw = ?";
+
+        try (Connection connection = DatabaseConnection.getConnection()){
+             PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, email);
+            preparedStatement.setString(2, password);
+
+            try(ResultSet rs = preparedStatement.executeQuery()) {
+                if (rs.next() && rs.getInt(1) > 0) {
+                    System.out.println("Accesso effettuato");
+                    return true; 
+                }
+                throw new SQLException();
+            } 
+            catch (SQLException e) {
+            e.printStackTrace();
+            Alert a = new Alert(AlertType.ERROR, "Errore durante l'accesso, campi inseriri non corretti, riprova");
+            a.setHeaderText("Errore");
+            a.setTitle("Errore durante l'accesso");
+            a.showAndWait();
+            mailTextField.clear();
+            passwordTextField.clear();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
